e07216ca64ad97da09af8639b61580d23ff97bfa
crislainecsoares <crislaines@dimed.com.br>
2021-08-08 17:10:17 -0300

    Adicionado anotacoes

--- src/main/java/com/example/demo/domain/Livro.java
+++ src/main/java/com/example/demo/domain/Livro.java
@@ -1,15 +1,23 @@
 package com.example.demo.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro() {
