e2e04d8fea5c48fae2959f5be7149232b09b8c33
crislainecsoares <crislaines@dimed.com.br>
2021-08-09 19:02:13 -0300

    Add padrao DTO

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.example.demo.resources;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.dto.CategoriaDTO;
 import com.example.demo.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -21,6 +25,13 @@ public class CategoriaResource {
         Categoria obj = categoriaService.findById(id);
         return ResponseEntity.ok().body(obj);
 
+    }
+
+    @GetMapping
+    public ResponseEntity<List< CategoriaDTO>> findAll() {
+        List<Categoria> list = categoriaService.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
