312ce89a58a4d7638c7aa748ad464f1e2cfbcea0
crislainecsoares <crislaines@dimed.com.br>
2021-08-09 16:45:45 -0300

    Add exception not_found

---
+++ src/main/java/com/example/demo/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.demo.exceptions;
+
+public class ObjectNotFoundException extends  RuntimeException{
+    public static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.exceptions.ObjectNotFoundException;
 import com.example.demo.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 }
