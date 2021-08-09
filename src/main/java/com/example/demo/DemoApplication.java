a86b152c65308cf82de7ad02b5457be29c690b23
crislainecsoares <crislaines@dimed.com.br>
2021-08-08 21:54:25 -0300

    Add test profile

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,27 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-	@Autowired // obter o CategoriaRepository que é gerado automaticamente pelo Spring
-	private CategoriaRepository categoriaRepository;
-	@Autowired // obter o LivroRepository que é gerado automaticamente pelo Spring
-	private LivroRepository livroRepository;
+public class DemoApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria categoria1 = new Categoria(null, "Casa", "Pato" );
-
-		Livro livro1 = new Livro(null, "My Book", "Somewhere", "Texto", categoria1);
-
-		categoria1.getLivros().addAll(Arrays.asList(livro1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(categoria1));
-		this.livroRepository.saveAll(Arrays.asList(livro1));
-
-	}
 }
---
+++ src/main/java/com/example/demo/service/DBService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Livro;
+import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired // obter o CategoriaRepository que é gerado automaticamente pelo Spring
+    private CategoriaRepository categoriaRepository;
+    @Autowired // obter o LivroRepository que é gerado automaticamente pelo Spring
+    private LivroRepository livroRepository;
+
+    public void DatabaseInstance() {
+
+        Categoria categoria1 = new Categoria(null, "Casa", "Pato" );
+
+        Livro livro1 = new Livro(null, "My Book", "Somewhere", "Texto", categoria1);
+
+        categoria1.getLivros().addAll(Arrays.asList(livro1));
+
+        this.categoriaRepository.saveAll(Arrays.asList(categoria1));
+        this.livroRepository.saveAll(Arrays.asList(livro1));
+
+    }
+}
